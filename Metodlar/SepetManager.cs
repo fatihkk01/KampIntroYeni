4d154f4ec9ce619433642f02e1aa606046b8d196
Fatih Kırık <fatih.kirik@bilgiedu.net>
2021-01-17 01:05:18 +0300

    Add project files.

---
+++ Metodlar/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodlar
+{
+    class SepetManager
+    {
+        //Naming Convention - İsimlendirme Kuralı
+        //Syntax - Yazım Şekilleri 
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urunAdi);
+        }
+    }
+}
