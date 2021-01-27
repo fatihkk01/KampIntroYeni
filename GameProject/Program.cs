04c6b4a9e8443f55d60c681e5b1858f8f6e7ccdc
Fatih Kırık <fatih.kirik@bilgiedu.net>
2021-01-28 00:24:05 +0300

    6. Ders SQL

---
+++ GameProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        //Not: Önce varlığı daha sonra operasyonları oluştur.
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+
+            gamerManager.Add(new Gamer { Id=1, BirthYear=1984, FirstName="Engin", LastName="Demiroğ", IdentityNumber=12345});
+
+        }
+    }
+}
